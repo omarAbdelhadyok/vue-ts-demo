1f67e8ac977b7c2dab547c7d62d268f42eb8bdc6
Omar Abdelhady <omar.abdelhady.ok@gmail.com>
2021-11-16 09:06:08 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(Quasar, quasarUserOptions)
+    .use(router)
+    .mount('#app');
