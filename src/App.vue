3f08c9cf0456b97504c25e6b92a82e98e074ec01
Omar Abdelhady <omar.abdelhady.ok@gmail.com>
2021-11-16 10:03:28 +0300

    minor setup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,67 @@
 <template>
-  <div>
-    
-  </div>
+	<!-- this is using quasar itself not tabadul component libary, just to test quasar in this setup with TS -->
+	<!-- the dev folder in the tabadul-ui-components is an application that is puropse is to try the components generated with tabadul-ui-components -->
+	<form>
+		<q-input
+			outlined
+			:label="t('labels.firstName')"
+			:hint="t('labels.firstNameHint')"
+			:hide-hint="true"
+			:counter="true"
+			v-model="v$.form.firstName.$model"
+            :errors="v$.form.firstName.$errors"
+			:error="v$.form.firstName.$errors[0] ? true : false"
+			:error-message="v$.form.firstName.$errors[0] ? t(v$.form.firstName.$errors[0].$message) : null">
+
+			<template v-slot:append>
+				<q-icon
+					name="cancel"
+					class="cursor-pointer"
+					@click.stop="form.firstName = ''"
+				>
+				</q-icon>
+			</template>
+		</q-input>
+	</form>
 </template>
 
 <script lang="ts">
+import { reactive, toRefs } from '@vue/reactivity';
+import { useI18n } from "vue-i18n/index";
+import useVuelidate from "@vuelidate/core";
 
 export default {
-  name: 'App',
-  setup (): unknown {
+	name: "App",
+	setup(): unknown {
+		const { t } = useI18n({ useScope: 'global' });
+		const state = reactive({
+			form: {
+				firstName: "first name"
+			}
+		});
 
-    
-    return {
-      
-    }
-  }
-}
+		const rules = {
+			form: {
+				firstName: {
+					required: {
+						$validator: function isRequired(value: string) {
+							if (value == null || value.trim().length === 0)
+								return false;
+							return true;
+						},
+						$message: "errors.required",
+					},
+				},
+			},
+		};
+
+		const v$ = useVuelidate(rules, state);
+
+		return {
+			...toRefs(state),
+			t,
+			v$,
+		};
+	},
+};
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,30 @@ import App from './App.vue'
 import router from './router'
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
+import { createI18n } from 'vue-i18n/index'
+
 
 const app = createApp(App);
 
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: {
+        ar: {},
+        en: {
+            labels: {
+                firstName: "First name",
+                firstNameHint: "First name hint"
+            },
+            errors: {
+                required: "required"
+            }
+        }
+    }
+
+});
+
 app.use(Quasar, quasarUserOptions)
     .use(router)
+    .use(i18n)
     .mount('#app');
